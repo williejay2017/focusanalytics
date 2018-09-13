9ebea995c1d39090da4b275172d04e6c3cecf386
Josh <williejay2009@gmail.com>
2018-09-12 23:41:59 -0500

    testing for jenkins

---
+++ src/ControlPanel.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15.4';
+import ControlPanel from './ControlPanel.js';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Control Panel component renders properly', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<ControlPanel />);
+  });
+  it('Correctly renders the three toggle switches', () => {
+    expect(wrapper.find('ToggleSwitch').exists()).toBe(true);
+  });
+});
