52598ff826b39ce4621b0bd36f754b60e59c3bc3
Josh <williejay2009@gmail.com>
2018-09-13 00:23:03 -0500

    removed dead code

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 import Calendar from './Calendar.js';
 import Heatmap from './Heatmap.js';
-// var data = [{x:2,y:4,value:2}];
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
